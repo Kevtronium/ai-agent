65cac4fa90d7ea77dc579d871d752f26617baae9
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2025-07-21 22:13:41 -0700

    implement get_files_info function

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
+    
+    try:
+        dir_contents = os.listdir(target_dir)
+        files_info = ""
+
+        for obj in dir_contents:
+            obj_path = os.path.join(target_dir, obj)
+            info_row = f'- {obj}: file_size={os.path.getsize(obj_path)} bytes, is_dir={os.path.isdir(obj_path)}\n'
+            files_info += info_row
+        
+        return files_info
+    except Exception as e:
+        return f"Error: {e}"
