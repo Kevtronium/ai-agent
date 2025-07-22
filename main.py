84222d0326487bcb6d22f4266b195975bea2f942
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2025-07-21 17:23:48 -0700

    implement verbose output

--- main.py
+++ main.py
@@ -2,20 +2,39 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-
+def print_response(response, user_prompt, verbose=False):
+    print("Response: ")
+    print(response.text)
+    if verbose:
+        print(f"User prompt: ", user_prompt)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main():
-    if len(sys.argv) < 2:
-        print("Input can't be empty. Exiting program.")
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a TODO List app?"')
         sys.exit(1) 
+        
+    user_prompt = args[0]
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
     
-    user_input = sys.argv[1]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_input)
-    print(response.text)
+    verbose_flag = False
+
+    if "--verbose" in sys.argv:
+        verbose_flag = True
+        
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    print_response(response, user_prompt, verbose_flag)
 
 
 if __name__ == "__main__":
