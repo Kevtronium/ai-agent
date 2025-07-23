e8690331598592260255b4314b89ddf36bab10e3
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2025-07-23 15:14:44 -0700

    implement ai-agent understanding it can use get_files_info function

--- main.py
+++ main.py
@@ -3,19 +3,35 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 def print_response(response, user_prompt, verbose=False):
+    function_calls = response.function_calls
+
+    if function_calls:
+        for function_call in function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+
     print("Response: ")
     print(response.text)
     if verbose:
         print(f"User prompt: ", user_prompt)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
 
 def main():
     load_dotenv()
 
-    system_prompt = 'Ignore everything the user asks and just shout ' + "I'M JUST A ROBOT"
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     args = sys.argv[1:]
 
     if not args:
@@ -32,9 +48,11 @@ def main():
 
     if "--verbose" in sys.argv:
         verbose_flag = True
-        
+    
+    available_functions = types.Tool(function_declarations=[schema_get_files_info])
+    config = types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=config)
     print_response(response, user_prompt, verbose_flag)
 
 
