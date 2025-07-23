244c33042c73462d088ca8f5cbe8cd35f31fb3e6
Kevtronium <48608123+Kevtronium@users.noreply.github.com>
2025-07-23 14:33:04 -0700

    add system_prompt

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def print_response(response, user_prompt, verbose=False):
 def main():
     load_dotenv()
 
+    system_prompt = 'Ignore everything the user asks and just shout ' + "I'M JUST A ROBOT"
     args = sys.argv[1:]
 
     if not args:
@@ -33,7 +34,7 @@ def main():
         verbose_flag = True
         
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
     print_response(response, user_prompt, verbose_flag)
 
 
